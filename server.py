ebe456a79087229c47573676eb82c5855d212642
Christina <Prinzessin@localhost.localdomain>
2019-11-19 16:16:15 +0000

    installed socktio and tried out ports - permission denied, need to check with board

--- server.py
+++ server.py
@@ -1,35 +1,48 @@
-# from gevent import monkey
-# monkey.patch_all()
+from gevent import monkey
+monkey.patch_all()
 
-# import serial
-# import time
-# from threading import Thread
+import serial #install pySerial
+import time
+import threading
 
 from flask import Flask, render_template
-# from flask.ext.socketio import SocketIO, emit, join_room, leave_room
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
+# Flask Setup with SocketIO
 app = Flask(__name__)
 app.debug = True
-# app.configure['SECRET_KEY'] = 'nomnomnom'
-# socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'nomnomnom'
+socketio = SocketIO(app)
 # thread = None
 
-'''port = '/dev/ttyS0'
-ser = serial.Serial(port, 115200, timeout = 0)
+port = '/dev/sg0'
+ser = serial.Serial(port, 115200, timeout = 0.5)
+# 9600 baud rate, '/dev/tty.usbserial', ttyUSB0
+# Ports: https://pyserial.readthedocs.io/en/latest/shortintro.html
 
-def background_thread():
+# background thread
+def serial_write_thread():
     while True:
         data = ser.read(9999)
         if len(data) > 0:
-            socket.emit('message', {'data': data})
+            socketio.emit('message', {'data': data})
         ser.close()
-'''
+
+def serial_read_thread():
+    while True:
+        data = "switch on light"
+        ser.write(data)
+        time.sleep(3)
+        # ser.flush()
+        ser.close()
+
+
 @app.route("/")
 def mesh_dashboard():
-    '''global thread
+    global thread
     if thread is None:
-        thread = Thread(target=background_thread)
-        thread.start()'''
+        thread = threading.Thread(target=serial_write_thread())
+        thread.start()
     return render_template('index.html')
 
 '''
@@ -45,6 +58,6 @@ def mesh_disconnect():
 '''
 
 if __name__ == "__main__":
-    #socketio.run(app)
-    app.run()
+    socketio.run(app)
+    # app.run()
     # app.run(host = "0.0.0.0", port = port)
