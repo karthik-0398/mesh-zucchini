c5d759a21713a1650ac80510a39d7b1062ca5020
Christina <Prinzessin@localhost.localdomain>
2019-11-25 16:52:05 +0000

    adapted a flask/bootstrap template

--- server.py
+++ server.py
@@ -5,44 +5,49 @@ import serial #install pySerial
 import time
 import threading
 
+from flask_bootstrap import Bootstrap
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
 # Flask Setup with SocketIO
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 app.debug = True
 app.config['SECRET_KEY'] = 'nomnomnom'
 socketio = SocketIO(app)
-# thread = None
+thread = None
 
-port = '/dev/sg0'
-ser = serial.Serial(port, 115200, timeout = 0.5)
+#port = '/dev/sdb' # '/dev/ttyS1' #
+#ser = serial.Serial(port, 115200, timeout = 0.5)
 # 9600 baud rate, '/dev/tty.usbserial', ttyUSB0
 # Ports: https://pyserial.readthedocs.io/en/latest/shortintro.html
 
 # background thread
 def serial_write_thread():
     while True:
-        data = ser.read(9999)
-        if len(data) > 0:
-            socketio.emit('message', {'data': data})
-        ser.close()
+        time.sleep(3)
+        #data = ser.read(9999)
+        #if len(data) > 0:
+        #    socketio.emit('message', {'data': data})
+        #ser.close()
 
 def serial_read_thread():
     while True:
         data = "switch on light"
-        ser.write(data)
+        #ser.write(data)
         time.sleep(3)
         # ser.flush()
-        ser.close()
+        #ser.close()
 
 
 @app.route("/")
+@app.route("/index.html")
 def mesh_dashboard():
-    global thread
-    if thread is None:
-        thread = threading.Thread(target=serial_write_thread())
-        thread.start()
+    #global thread
+    #if thread is None:
+    #    thread = threading.Thread(target=serial_write_thread())
+    #    thread.start()
     return render_template('index.html')
 
 '''
@@ -59,5 +64,6 @@ def mesh_disconnect():
 
 if __name__ == "__main__":
     socketio.run(app)
+    # app.run()
     # app.run()
     # app.run(host = "0.0.0.0", port = port)
