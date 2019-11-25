5821ef32cc98a4170f1b8a64e658e821d445b150
Christina <Prinzessin@localhost.localdomain>
2019-11-25 18:19:09 +0000

    not working at the moment

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from gevent import monkey
 monkey.patch_all()
 
-import serial #install pySerial
+# import serial #install pySerial
 import time
 import threading
 
@@ -15,26 +15,26 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.debug = True
 app.config['SECRET_KEY'] = 'nomnomnom'
-socketio = SocketIO(app)
+# socketio = SocketIO(app)
 thread = None
 
-#port = '/dev/sdb' # '/dev/ttyS1' #
-#ser = serial.Serial(port, 115200, timeout = 0.5)
+# port = '/dev/ttyACM0' # '/dev/ttyS1' #
+# ser = serial.Serial(port, 115200, timeout = 0.5)
 # 9600 baud rate, '/dev/tty.usbserial', ttyUSB0
 # Ports: https://pyserial.readthedocs.io/en/latest/shortintro.html
 
 # background thread
-def serial_write_thread():
-    while True:
-        time.sleep(3)
-        #data = ser.read(9999)
-        #if len(data) > 0:
-        #    socketio.emit('message', {'data': data})
-        #ser.close()
-
 def serial_read_thread():
     while True:
-        data = "switch on light"
+        time.sleep(3)
+       # data = ser.read(9999)
+        #if len(data) > 0:
+     #       socketio.emit('message', {'data': data})
+    #    ser.close()
+
+def serial_write_thread():
+    while True:
+        #data = "010101"
         #ser.write(data)
         time.sleep(3)
         # ser.flush()
@@ -44,18 +44,18 @@ def serial_read_thread():
 @app.route("/")
 @app.route("/index.html")
 def mesh_dashboard():
-    #global thread
-    #if thread is None:
-    #    thread = threading.Thread(target=serial_write_thread())
-    #    thread.start()
+    global thread
+    if thread is None:
+        thread = threading.Thread(target=serial_write_thread())
+        thread.start()
     return render_template('index.html')
 
-'''
+
 @app.route("/connect/")
 def mesh_connect():
     join_room("one")
     return "Connect"
-
+'''
 @app.route("/disconnect/")
 def mesh_disconnect():
     leave_room("one")
@@ -63,7 +63,7 @@ def mesh_disconnect():
 '''
 
 if __name__ == "__main__":
-    socketio.run(app)
-    # app.run()
+    # socketio.run(app)
+    app.run()
     # app.run()
     # app.run(host = "0.0.0.0", port = port)
