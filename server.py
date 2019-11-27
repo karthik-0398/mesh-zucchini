86bca7839dce23fcfa656767eb65719d186198c6
Christina <Prinzessin@localhost.localdomain>
2019-11-27 18:56:47 +0000

    just print message read from serial, if data length is bigger than zerO

--- server.py
+++ server.py
@@ -62,13 +62,13 @@ def serial_read_thread():
     while True:
         # data = ser.read(5)
         try:
-            line = ser.read(10).decode('utf-8')
+            data = ser.read(10).decode('utf-8')
         except IOError:
             print("Error")
-        messages.append(line)
-        print("Message", line)
-        #if len(data) > 0:
+        if len(data) > 0:
         #    socketio.emit('message', {'data': data})
+            messages.append(data)
+            print("Message", data)
         time.sleep(3)
     #ser.flush()
     ser.close()
@@ -143,49 +143,3 @@ if __name__ == "__main__":
     # app.run()
     # app.run()
     # app.run(host = "0.0.0.0", port = port)
-
-"""
-from gevent import monkey
-monkey.patch_all()
-
-import serial
-import time
-from threading import Thread
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, join_room, leave_room
-
-app = Flask(__name__)
-app.debug = True
-app.config['SECRET_KEY'] = 'hihihihi'
-socketio = SocketIO(app)
-thread = None
-
-port = '/dev/ttyS0'
-ser = serial.Serial(port, 115200, timeout=0)
-
-def background_thread():
-    while True:
-        data = ser.read(9999)
-        if len(data) > 0:
-            socketio.emit('message', {'data': data}, namespace='/test')
-    ser.close()
-
-@app.route('/')
-def index():
-    global thread
-    if thread is None:
-        thread = Thread(target=background_thread)
-        thread.start()
-    return render_template('index2.html')
-
-@socketio.on('join', namespace='/test')
-def join(message):
-    join_room(message['room'])
-
-@socketio.on('leave', namespace='/test')
-def leave(message):
-    leave_room(message['room'])
-
-if __name__ == '__main__':
-    socketio.run(app)
-"""
