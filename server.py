61a9a84aeb8e10b1f5fdce3e7ff1e6398d24ba7b
Christina <Prinzessin@localhost.localdomain>
2019-12-06 19:49:24 +0000

    wooooooooooorking!!! the leds can now be switched on by the website

--- server.py
+++ server.py
@@ -7,19 +7,27 @@ import threading
 
 from flask_bootstrap import Bootstrap
 
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, json
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
 from dataclasses import dataclass
 
+
+
 # Flask Setup with SocketIO
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-app.debug = True
+app.debug = False
 app.config['SECRET_KEY'] = 'nomnomnom'
 socketio = SocketIO(app)
 thread = None
 thread2 = None
+thread3 = None
+light_command = "None"
+light_command_old = "None"
+
+finished = False
 
 board_list = []
 
@@ -41,61 +49,91 @@ def init_boards():
     board_list.append(board3)
     board_list.append(board4)
 
-messages = ["Connected", "Disconnected", "Conneadasdaacted", "Disconnected", "Connected", "Disconnected", "Connected"]
+messages = []
 
-# port = '/dev/ttyACM0' # '/dev/ttyS1' #
-port = '/dev/ttyS1' #
+port = '/dev/ttyACM1' # '/dev/ttyS1' #
+# port = '/dev/ttyS4' #
 ser = serial.Serial(port, 115200, timeout = 0.5, rtscts=True,dsrdtr=True)
 ser.close()
-port2 = '/dev/ttyS0' #
-ser2 = serial.Serial(port2, 115200, timeout = 0.5, rtscts=True,dsrdtr=True)
-ser2.close()
+#port2 = '/dev/ttyS2' #
+#ser2 = serial.Serial(port2, 115200, timeout = 0.5, rtscts=True,dsrdtr=True)
+#ser2.close()
 # 9600 baud rate, '/dev/tty.usbserial', ttyUSB0
 # Ports: https://pyserial.readthedocs.io/en/latest/shortintro.html
 
+
 # todo, when other data is read, then there is a bottleneck
 # try to avoid messages that are not in the right format
-
 # background thread
 def serial_read_thread():
+    global light_command_old
     ser.open()
+    data = ""
     while True:
         # data = ser.read(5)
         try:
-            data = ser.read(10).decode('utf-8')
+            data = ser.read(30).decode('utf-8')
         except IOError:
             print("Error")
         if len(data) > 0:
-        #    socketio.emit('message', {'data': data})
-            messages.append(data)
-            print("Message", data)
-        time.sleep(3)
-    #ser.flush()
+            # socketio.emit('message', {'messages': data})
+            if(data != "None"):
+                messages.append("Received: " + data)
+            # print("Message", data)
+        time.sleep(2)
+        data = ""
+        if (light_command_old != light_command):
+            ser.write(bytes(light_command[0].encode('utf-8')))  # b'lalala
+            light_command_old = light_command
+            messages.append("Sent: " + light_command)
+            # ser.write(b'\r')
+        time.sleep(2)
+    ser.flush()
     ser.close()
 
+
 # writes hihihi every 3 seconds
-def serial_write_thread():
+"""def serial_write_thread():
+    global light_command_old
     ser2.open()
     while True:
-        data = "010101"
-        ser2.write(bytes(b'hihihi'))
-        time.sleep(3)
+        if(light_command_old != light_command):
+            ser2.write(bytes(light_command.encode('utf-8'))) # b'lalala
+            light_command_old = light_command
+            messages.append("Sent: " + light_command)
+            ser2.write(b'\r')
+        time.sleep(2)
     #ser2.flush()
-    ser2.close()
+    ser2.close()"""
 
-
-@app.route("/")
+@app.route("/",  methods=['GET', 'POST'])
 @app.route("/index.html")
 def mesh_dashboard():
+
+    global light_command
+    print(request.method)
+
+    if request.method == 'POST':
+        light_command = str(request.form.get('colour_button'))
+        finished = False
+
+    print(light_command)
+
     global thread
     if thread is None:
         thread = threading.Thread(target=serial_read_thread)
         thread.start()
-    global thread2
-    if thread2 is None:
-        thread2 = threading.Thread(target=serial_write_thread)
-        thread2.start()
-    time.sleep(5)
+
+  #  global thread2
+  #  if thread2 is None:
+  #      thread2 = threading.Thread(target=serial_write_thread)
+   #     thread2.start()
+
+#    global thread3
+#    if thread3 is None:
+#        thread3 = threading.Thread(target=button_pressed)
+#        thread3.start()
+
     boards = [
         {
             'name': 'Leader Board',
